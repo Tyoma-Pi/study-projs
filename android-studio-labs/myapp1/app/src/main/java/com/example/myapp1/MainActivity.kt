7df8dfefb4fd134692e79a4d197ef74501244ab2
Tyoma-Pi <artyom.pischulin@gmail.com>
2023-03-04 14:28:50 +0500

    Hey! New updates

--- android-studio-labs/myapp1/app/src/main/java/com/example/myapp1/MainActivity.kt
+++ android-studio-labs/myapp1/app/src/main/java/com/example/myapp1/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.myapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        myText1.setText("Hello, world")
+        val myText: TextView = findViewById(R.id.myText1)
+        val editText: TextView = findViewById(R.id.editText)
+        var str: String = editText.text.toString()
+        myText.text = str
     }
 }
