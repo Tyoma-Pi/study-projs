73a221a820f6576e093569f1f1db81079304d359
Tyoma-Pi <artyom.pischulin@gmail.com>
2023-03-30 17:01:48 +0500

    Hey, updates! 	modified:   android-studio-labs/myproj2/app/build.gradle 	modified:   android-studio-labs/myproj2/app/src/main/java/com/example/my_proj2/MainActivity.kt 	deleted:    android-studio-labs/myproj3/.gitignore 	deleted:    android-studio-labs/myproj3/.idea/.gitignore 	deleted:    android-studio-labs/myproj3/.idea/.name 	deleted:    android-studio-labs/myproj3/.idea/compiler.xml 	deleted:    android-studio-labs/myproj3/.idea/dictionaries/Tyoma.xml 	deleted:    android-studio-labs/myproj3/.idea/gradle.xml 	deleted:    android-studio-labs/myproj3/.idea/misc.xml 	deleted:    android-studio-labs/myproj3/.idea/vcs.xml 	deleted:    android-studio-labs/myproj3/app/.gitignore 	deleted:    android-studio-labs/myproj3/app/build.gradle 	deleted:    android-studio-labs/myproj3/app/proguard-rules.pro 	deleted:    android-studio-labs/myproj3/app/src/androidTest/java/com/example/my_proj3/ExampleInstrumentedTest.kt 	deleted:    android-studio-labs/myproj3/app/src/main/AndroidManifest.xml 	deleted:    android-studio-labs/myproj3/app/src/main/java/com/example/my_proj3/MainActivity.kt 	deleted:    android-studio-labs/myproj3/app/src/main/res/drawable-v24/ic_launcher_foreground.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/drawable/ic_launcher_background.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/drawable/numbers.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/drawable/operators_a.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/drawable/operators_m.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/layout/activity_main.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-anydpi-v33/ic_launcher.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-hdpi/ic_launcher.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-hdpi/ic_launcher_round.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-mdpi/ic_launcher.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-xhdpi/ic_launcher.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-xhdpi/ic_launcher_round.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-xxhdpi/ic_launcher.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-xxxhdpi/ic_launcher.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp 	deleted:    android-studio-labs/myproj3/app/src/main/res/values-night/themes.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/values/colors.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/values/strings.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/values/styles.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/values/themes.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/xml/backup_rules.xml 	deleted:    android-studio-labs/myproj3/app/src/main/res/xml/data_extraction_rules.xml 	deleted:    android-studio-labs/myproj3/app/src/test/java/com/example/my_proj3/ExampleUnitTest.kt 	deleted:    android-studio-labs/myproj3/build.gradle 	deleted:    android-studio-labs/myproj3/gradle.properties 	deleted:    android-studio-labs/myproj3/gradle/wrapper/gradle-wrapper.jar 	deleted:    android-studio-labs/myproj3/gradle/wrapper/gradle-wrapper.properties 	deleted:    android-studio-labs/myproj3/gradlew 	deleted:    android-studio-labs/myproj3/gradlew.bat 	deleted:    android-studio-labs/myproj3/settings.gradle

--- android-studio-labs/myproj2/app/src/main/java/com/example/my_proj2/MainActivity.kt
+++ android-studio-labs/myproj2/app/src/main/java/com/example/my_proj2/MainActivity.kt
@@ -3,22 +3,18 @@ package com.example.my_proj2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
+import com.example.my_proj2.databinding.ActivityMainBinding
 import kotlin.math.abs
 import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val elemA: TextView = findViewById(R.id.formulaEl1)
-        val elemB: TextView = findViewById(R.id.formulaEl2)
-        val elemC: TextView = findViewById(R.id.formulaEl3)
-        val myRes: TextView = findViewById(R.id.myResult)
-        val countX: View = findViewById(R.id.countX)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         fun checkD(myD: Double): Int {
             return if (myD > 0.0) 1
@@ -27,50 +23,49 @@ class MainActivity : AppCompatActivity() {
         }
 
         fun doWork() {
-            myRes.setTextColor(AppCompatResources.getColorStateList(this, R.color.black))
+            binding.myResult.setTextColor(AppCompatResources.getColorStateList(this, R.color.black))
 
-            val a: Double = elemA.text.toString().toDouble()
-            val b: Double = elemB.text.toString().toDouble()
-            val c: Double = elemC.text.toString().toDouble()
+            val a: Double = binding.formulaEl1.text.toString().toDouble()
+            val b: Double = binding.formulaEl2.text.toString().toDouble()
+            val c: Double = binding.formulaEl3.text.toString().toDouble()
             val descr = (b * b - 4 * a * c)
 
             when (checkD(descr)) {
                 1 -> {
                     if (a == 0.0) {
                         val x = -c / b
-                        myRes.text = "Линейное уравнение,\nx = $x"
+                        binding.myResult.text = "Линейное уравнение,\nx = $x"
                     }
                     else {
                         val x1 = String.format("%.3f", ((-b + sqrt(descr)) / (2 * a))).toDouble()
                         val x2 = String.format("%.3f", ((-b - sqrt(descr)) / (2 * a))).toDouble()
-                        myRes.text = "x1 = $x1\nx2 = + $x2"
+                        binding.myResult.text = "x1 = $x1\nx2 = $x2"
                     }
                 }
                 0 -> {
                     if (a == 0.0 || c == 0.0 && b == 0.0) {
-                        myRes.setTextColor(AppCompatResources.getColorStateList(this, R.color.red))
-                        myRes.text = "На ноль делить нельзя"
+                        binding.myResult.setTextColor(AppCompatResources.getColorStateList(this, R.color.red   ))
+                        binding.myResult.text = "На ноль делить нельзя"
                     }
                     else {
                         val x = String.format("%.3f", (-b / (2 * a))).toDouble()
-                        myRes.text = "x1 = x2 = ${abs(x)}"
+                        binding.myResult.text = "x1 = x2 = ${abs(x)}"
                     }
                 }
                 -1 -> {
-                    myRes.text = "Корней нет"
+                    binding.myResult.text = "Корней нет"
                 }
             }
         }
 
-        elemC.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+        binding.formulaEl3.setOnKeyListener{ _, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
                 doWork()
-                return@OnKeyListener true
             }
             false
-        })
+        }
 
-        countX.setOnClickListener {
+        binding.countX.setOnClickListener {
             doWork()
         }
     }
