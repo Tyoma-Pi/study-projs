92b5ecb80eb50069dd8a1fd65de4b8c8803cbeff
Tyoma-Pi <artyom.pischulin@gmail.com>
2023-05-28 00:05:39 +0500

    Hello, updates! 	modified:   android-studio-labs/myproj3/app/src/main/res/layout/activity_main.xml 	modified:   android-studio-labs/myproj3/app/src/main/java/com/example/my_proj3/MainActivity.kt 	modified:   android-studio-labs/myproj3/app/src/main/res/values/styles.xml

--- android-studio-labs/myproj3/app/src/main/java/com/example/my_proj3/MainActivity.kt
+++ android-studio-labs/myproj3/app/src/main/java/com/example/my_proj3/MainActivity.kt
@@ -7,7 +7,11 @@ import android.graphics.drawable.Drawable
 import android.graphics.drawable.TransitionDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import com.example.my_proj3.databinding.ActivityMainBinding
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,91 +32,339 @@ class MainActivity : AppCompatActivity() {
                 )
         )
 
-        var isOperated = false
-        var myOperation: String
-        var temp: Double
+        var num1 = 0.0
+        var num2 = 0.0
+        var numFract1 = 0
+        var numFract2 = 0
+        var numFractF = 0
+        var forDigits = false
+        var forEquation = false
+        var error = false
+        var myOperation = ""
 
-        binding.butC.setOnClickListener {
-            binding.result.text = "0"
-            isOperated = false
-            myOperation = ""
-            temp = 0.0
-            binding.butDV.setBackgroundResource(R.drawable.operators_m)
-            binding.butDV.setTextColor(Color.parseColor("#FFFFFFFF"))
-        }
-        binding.but0.setOnClickListener {
-            if (binding.result.text != "0") {
-                binding.result.text = "${binding.result.text}0"
+        binding.apply {
+            fun defTSize()
+            {
+                if (result.text.toString().length > 17)
+                    result.setTextSize(22F)
+                else if (result.text.toString().length > 14)
+                    result.setTextSize(30F)
+                else if (result.text.toString().length > 11)
+                    result.setTextSize(36F)
+                else
+                    result.setTextSize(45F)
             }
-        }
-        binding.but1.setOnClickListener {
-            binding.result.text = if (binding.result.text == "0" || isOperated) "1"
-            else binding.result.text.toString() + "1"
-            isOperated = false
-        }
-        binding.but2.setOnClickListener {
-            binding.result.text = if (binding.result.text == "0" || isOperated) "2"
-            else binding.result.text.toString() + "2"
-            isOperated = false
-        }
-        binding.but3.setOnClickListener {
-            binding.result.text = if (binding.result.text == "0" || isOperated) "3"
-            else binding.result.text.toString() + "3"
-            isOperated = false
-        }
-        binding.but4.setOnClickListener {
-            binding.result.text = if (binding.result.text == "0" || isOperated) "4"
-            else binding.result.text.toString() + "4"
-            isOperated = false
-        }
-        binding.but5.setOnClickListener {
-            binding.result.text = if (binding.result.text == "0" || isOperated) "5"
-            else binding.result.text.toString() + "5"
-            isOperated = false
-        }
-        binding.but6.setOnClickListener {
-            binding.result.text = if (binding.result.text == "0" || isOperated) "6"
-            else binding.result.text.toString() + "6"
-            isOperated = false
-        }
-        binding.but7.setOnClickListener {
-            binding.result.text = if (binding.result.text == "0" || isOperated) "7"
-            else binding.result.text.toString() + "7"
-            isOperated = false
-        }
-        binding.but8.setOnClickListener {
-            binding.result.text = if (binding.result.text == "0" || isOperated) "8"
-            else binding.result.text.toString() + "8"
-            isOperated = false
-        }
-        binding.but9.setOnClickListener {
-            binding.result.text = if (binding.result.text == "0" || isOperated) "9"
-            else binding.result.text.toString() + "9"
-            isOperated = false
-        }
-        binding.butD.setOnClickListener {
-            if (!binding.result.text.contains(',')) {
-                binding.result.text = "${binding.result.text},"
+
+            butC.setOnClickListener {
+                result.text = "0"
+                num1 = 0.0
+                num2 = 0.0
+                numFract1 = 0
+                forDigits = false
+                forEquation = false
+                error = false
+                myOperation = ""
+                butDV.setBackgroundResource(R.drawable.operators_m)
+                butDV.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butML.setBackgroundResource(R.drawable.operators_m)
+                butML.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butMN.setBackgroundResource(R.drawable.operators_m)
+                butMN.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butPL.setBackgroundResource(R.drawable.operators_m)
+                butPL.setTextColor(Color.parseColor("#FFFFFFFF"))
+                result.setTextSize(45F)
             }
-        }
-        binding.butPM.setOnClickListener {
-            temp = binding.result.text.toString().replace(',', '.').toDouble()
-            binding.result.text = if (temp < 0.0) binding.result.text.drop(1)
-            else if (temp == 0.0) binding.result.text
-            else "-${binding.result.text}"
-        }
-        binding.butPR.setOnClickListener {
-                binding.result.text = "${binding.result.text}%"
-        }
-        binding.butDV.setOnClickListener {
-//            binding.butDV.requestFocus()
-//            binding.butDV.backgroundTintMode = myMBBtransition
-            myMBBtransition.startTransition(400)
-            binding.butDV.setBackgroundResource(R.drawable.operators_m_a)
-            binding.butDV.setTextColor(Color.parseColor("#FFFF9600"))
-            isOperated = true
-            myOperation = "/"
-            temp = binding.result.text.toString().toDouble()
+
+            but0.setOnClickListener {
+                if (forDigits)
+                {
+                    result.text = "0"
+                }
+                else if (result.text.contains("Error"))
+                {
+                    result.text = "Error"
+                }
+                else if (result.text != "0") {
+                    result.text = "${result.text}0"
+                }
+                forDigits = false
+                defTSize()
+            }
+            but1.setOnClickListener {
+                result.text = if (result.text == "0" || forDigits) "1"
+                else if (result.text.contains("Error")) "Error"
+                else result.text.toString() + "1"
+                forDigits = false
+                defTSize()
+            }
+            but2.setOnClickListener {
+                result.text = if (result.text == "0" || forDigits) "2"
+                else if (result.text.contains("Error")) "Error"
+                else result.text.toString() + "2"
+                forDigits = false
+                defTSize()
+            }
+            but3.setOnClickListener {
+                result.text = if (result.text == "0" || forDigits) "3"
+                else if (result.text.contains("Error")) "Error"
+                else result.text.toString() + "3"
+                forDigits = false
+                defTSize()
+            }
+            but4.setOnClickListener {
+                result.text = if (result.text == "0" || forDigits) "4"
+                else if (result.text.contains("Error")) "Error"
+                else result.text.toString() + "4"
+                forDigits = false
+                defTSize()
+            }
+            but5.setOnClickListener {
+                result.text = if (result.text == "0" || forDigits) "5"
+                else if (result.text.contains("Error")) "Error"
+                else result.text.toString() + "5"
+                forDigits = false
+                defTSize()
+            }
+            but6.setOnClickListener {
+                result.text = if (result.text == "0" || forDigits) "6"
+                else if (result.text.contains("Error")) "Error"
+                else result.text.toString() + "6"
+                forDigits = false
+                defTSize()
+            }
+            but7.setOnClickListener {
+                result.text = if (result.text == "0" || forDigits) "7"
+                else if (result.text.contains("Error")) "Error"
+                else result.text.toString() + "7"
+                forDigits = false
+                defTSize()
+            }
+            but8.setOnClickListener {
+                result.text = if (result.text == "0" || forDigits) "8"
+                else if (result.text.contains("Error")) "Error"
+                else result.text.toString() + "8"
+                forDigits = false
+                defTSize()
+            }
+            but9.setOnClickListener {
+                result.text = if (result.text == "0" || forDigits) "9"
+                else if (result.text.contains("Error")) "Error"
+                else result.text.toString() + "9"
+                forDigits = false
+                defTSize()
+            }
+            butD.setOnClickListener {
+                if (result.text.contains("Error"))
+                {
+                    result.text = "Error"
+                }
+                else if (!result.text.contains(','))
+                {
+                    result.text = "${result.text},"
+                }
+                defTSize()
+            }
+
+            butPM.setOnClickListener {
+                if (!error)
+                    if (!forEquation)
+                    {
+                        num1 = result.text.toString().replace(',', '.').toDouble()
+                    }
+                    else
+                    {
+                        num2 = result.text.toString().replace(',', '.').toDouble()
+                    }
+                result.text = if (result.text == "Error") "Error"
+                else if(result.text.toString().replace(',', '.').toDouble() < 0.0) result.text.drop(1)
+                else if (result.text.toString().replace(',', '.').toDouble() == 0.0) result.text
+                else "-${result.text}"
+                defTSize()
+            }
+            butPR.setOnClickListener {
+                result.text = if (result.text == "Error") "Error"
+                else if (result.text in listOf("0", "0,", "0,0") ||
+                    result.text.contains(Regex("^0,0+$"))) "0"
+                else (result.text.toString().replace(',', '.').toDouble() / 100)
+                    .toString().replace('.', ',')
+                defTSize()
+            }
+
+            butDV.setOnClickListener {
+//                butDV.requestFocus()
+//                butDV.backgroundTintMode = myMBBtransition
+//                myMBBtransition.startTransition(400)
+                if (forEquation && myOperation == "/")
+                {
+                    numFract1 = if (result.text == "0" || num1 == 0.0 && result.text == "Error" || error) 0
+                    else 0
+                    result.text = if (result.text == "0" || num1 == 0.0 && result.text == "Error" || error) "Error"
+                    else String.format("%.${numFract1}f", num1 / num1)
+                }
+                if (result.text != "Error")
+                {
+                    num1 = result.text.toString().replace(',', '.').toDouble()
+                    if (!forEquation)
+                        numFract1 = if (result.text.contains(",")) {
+                            if (result.text.startsWith("-")) result.text.drop(3).length
+                            else result.text.drop(2).length
+                        } else 0
+                }
+                else
+                    error = true
+                forDigits = true
+                forEquation = true
+                myOperation = "/"
+                butDV.setBackgroundResource(R.drawable.operators_m_a)
+                butDV.setTextColor(Color.parseColor("#FFFF9600"))
+                butML.setBackgroundResource(R.drawable.operators_m)
+                butML.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butMN.setBackgroundResource(R.drawable.operators_m)
+                butMN.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butPL.setBackgroundResource(R.drawable.operators_m)
+                butPL.setTextColor(Color.parseColor("#FFFFFFFF"))
+                defTSize()
+            }
+            butML.setOnClickListener {
+                if (forEquation && myOperation == "*")
+                {
+                    numFract1 = if (result.text == "0" || num1 == 0.0 && result.text == "Error" || error) 0
+                    else numFract1 * 2
+                    result.text = if (result.text == "Error" || error) "Error"
+                    else String.format("%.${numFract1}f", num1 * num1).replace('.', ',')
+                }
+                if (result.text != "Error")
+                {
+                    num1 = result.text.toString().replace(',', '.').toDouble()
+                    if (!forEquation)
+                        numFract1 = if (result.text.contains(",")) {
+                            if (result.text.startsWith("-")) result.text.drop(3).length
+                            else result.text.drop(2).length
+                        } else 0
+                }
+                else
+                    error = true
+                forDigits = true
+                forEquation = true
+                myOperation = "*"
+                butDV.setBackgroundResource(R.drawable.operators_m)
+                butDV.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butML.setBackgroundResource(R.drawable.operators_m_a)
+                butML.setTextColor(Color.parseColor("#FFFF9600"))
+                butMN.setBackgroundResource(R.drawable.operators_m)
+                butMN.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butPL.setBackgroundResource(R.drawable.operators_m)
+                butPL.setTextColor(Color.parseColor("#FFFFFFFF"))
+                defTSize()
+            }
+            butMN.setOnClickListener {
+                if (forEquation && myOperation == "-")
+                {
+                    numFract1 = 0
+                    result.text = if (result.text == "Error" || error) "Error"
+                    else String.format("%.${numFract1}f", num1 - num1)
+                }
+                if (result.text != "Error")
+                {
+                    num1 = result.text.toString().replace(',', '.').toDouble()
+                    if (!forEquation)
+                        numFract1 = if (result.text.contains(",")) {
+                            if (result.text.startsWith("-")) result.text.drop(3).length
+                            else result.text.drop(2).length
+                        } else 0
+                }
+                else
+                    error = true
+                forDigits = true
+                forEquation = true
+                myOperation = "-"
+                butDV.setBackgroundResource(R.drawable.operators_m)
+                butDV.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butML.setBackgroundResource(R.drawable.operators_m)
+                butML.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butMN.setBackgroundResource(R.drawable.operators_m_a)
+                butMN.setTextColor(Color.parseColor("#FFFF9600"))
+                butPL.setBackgroundResource(R.drawable.operators_m)
+                butPL.setTextColor(Color.parseColor("#FFFFFFFF"))
+                defTSize()
+            }
+            butPL.setOnClickListener {
+                if (forEquation && myOperation == "+")
+                {
+                    result.text = if (result.text == "Error" || error) "Error"
+                    else String.format("%.${numFract1}f", num1 + num1).replace('.', ',')
+                }
+                if (result.text != "Error")
+                {
+                    num1 = result.text.toString().replace(',', '.').toDouble()
+                    if (!forEquation)
+                        numFract1 = if (result.text.contains(",")) {
+                            if (result.text.startsWith("-")) result.text.drop(3).length
+                            else result.text.drop(2).length
+                        } else 0
+                }
+                else
+                    error = true
+                forDigits = true
+                forEquation = true
+                myOperation = "+"
+                butDV.setBackgroundResource(R.drawable.operators_m)
+                butDV.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butML.setBackgroundResource(R.drawable.operators_m)
+                butML.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butMN.setBackgroundResource(R.drawable.operators_m)
+                butMN.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butPL.setBackgroundResource(R.drawable.operators_m_a)
+                butPL.setTextColor(Color.parseColor("#FFFF9600"))
+                defTSize()
+            }
+
+            butEQ.setOnClickListener {
+                forDigits = false
+                if (forEquation && !error)
+                {
+                    num2 = result.text.toString().replace(',', '.').toDouble()
+                    numFract2 = if (result.text.contains(",")) {
+                        if (result.text.startsWith("-")) result.text.drop(3).length
+                        else result.text.drop(2).length
+                    } else 0
+                }
+                forEquation = false
+                if (error || myOperation == "/" && num2 == 0.0)
+                {
+                    result.text = "Error"
+                    error = true
+                }
+                else if (myOperation == "/" && num1 == 0.0)
+                    result.text = "0"
+                else
+                {
+                    numFractF = when (myOperation){
+                        "/" -> abs(numFract1 - numFract2)
+                        "*" -> numFract1 + numFract2
+                        else -> max(numFract1, numFract2)
+                    }
+                    result.text = when (myOperation) {
+                        "/" -> String.format("%.${numFractF}f", num1 / num2)
+                        "*" -> String.format("%.${numFractF}f", num1 * num2)
+                        "-" -> String.format("%.${numFractF}f", num1 - num2)
+                        "+" -> String.format("%.${numFractF}f", num1 + num2)
+                        else -> if (result.text.matches("^0,(0+)?$".toRegex())) "0"
+                            else result.text.toString()
+                    }.replace('.', ',').replace(",0", "")
+                    num1 = result.text.toString().replace(',', '.').toDouble()
+                }
+                butDV.setBackgroundResource(R.drawable.operators_m)
+                butDV.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butML.setBackgroundResource(R.drawable.operators_m)
+                butML.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butMN.setBackgroundResource(R.drawable.operators_m)
+                butMN.setTextColor(Color.parseColor("#FFFFFFFF"))
+                butPL.setBackgroundResource(R.drawable.operators_m)
+                butPL.setTextColor(Color.parseColor("#FFFFFFFF"))
+//                testOut.text = result.text.toString().length.toString()
+                defTSize()
+            }
         }
     }
 }
